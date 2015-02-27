c4da2d26f17c2f887d7affd53fcd378067bb40bd
Dylan Powers <dylan.kyle.powers@gmail.com>
2015-02-27 13:50:42 -0800

    Uhhhh, just in the middle of working

--- ext/main.dart
+++ ext/main.dart
@@ -1,33 +1,61 @@
 library ipfs_interceptor;
 
+import 'dart:async';
 import 'dart:html';
 import 'dart:js';
 
 void main() {
+  JsObject runtimeNamespace = context['chrome']['runtime'];
+  addListenerToChromeEvent(runtimeNamespace, 'onInstalled', onInstalledAction);
+
   var onBeforeRequest = context['chrome']['webRequest']['onBeforeRequest'];
   onBeforeRequest.callMethod('addListener', [
-    onBeforeRequestCallback,
+    webRequestOnBeforeRequestAction,
     new JsObject.jsify({
-      'urls': ['*://gateway.ipfs.io/ipfs/*', '*://gateway.ipfs.io/ipns/*']
+      'urls': ['http://gateway.ipfs.io/ipfs/*', 'http://gateway.ipfs.io/ipns/*']
     }),
     new JsObject.jsify(['blocking'])
   ]);
 
-  // Dart2JS glitch workaround https://code.google.com/p/dart/issues/detail?id=20800
-  var tabsOnUpdated = context['chrome']['tabs']['onUpdated'];
-  JsObject dartTabsOnUpdatedEvent;
-  if (tabsOnUpdated is JsObject) {
-    dartTabsOnUpdatedEvent = tabsOnUpdated;
-  } else {
-    dartTabsOnUpdatedEvent = new JsObject.fromBrowserObject(tabsOnUpdated);
-  }
+  JsObject pageActionNamespace = context['chrome']['pageAction'];
+  addListenerToChromeEvent(pageActionNamespace, 'onClicked', pageActionOnClickedAction);
 
-  dartTabsOnUpdatedEvent.callMethod('addListener', [
-    tabsOnUpdatedCallback
-  ]);
+  JsObject contextMenusNamespace = context['chrome']['contextMenus'];
+  addListenerToChromeEvent(contextMenusNamespace, 'onClicked', (JsObject info, JsObject tab) {
+    print('MenuId: ${info['menuItemId']}); url: ${info['linkUrl']}');
+  });
 }
 
-JsObject onBeforeRequestCallback(JsObject data) {
+void addContextMenuAction() {
+  JsObject tabsNamespace = context['chrome']['tabs'];
+  tabsNamespace.callMethod('getCurrent', [(JsObject tab) {
+    String url = tab['url'];
+    var urlOrigin = Uri.parse(url).origin;
+    var props = new JsObject.jsify({
+      'contexts': ['link'],
+      'title': 'Copy as IPFS URL',
+      'documentUrlPatterns': ['$urlOrigin/ipfs/*', '$urlOrigin/ipns/*']
+    });
+    JsObject contextMenusNamespace = context['chrome']['contextMenus'];
+    contextMenusNamespace.callMethod('create', [props]);
+  }]);
+
+}
+
+void addListenerToChromeEvent(JsObject namespace, String eventName, Function callback) {
+  var event = namespace[eventName];
+  JsObject dartifiedEvent;
+  if (event is JsObject) {
+    dartifiedEvent = event;
+  } else {
+    // Dart2JS glitch workaround https://code.google.com/p/dart/issues/detail?id=20800
+    dartifiedEvent = new JsObject.fromBrowserObject(event);
+  }
+
+  dartifiedEvent.callMethod('addListener', [callback]);
+}
+
+JsObject webRequestOnBeforeRequestAction(JsObject data) {
   var urlString = data['url'];
   var url = Uri.parse(urlString);
   url = new Uri.http('localhost:8080', url.path);
@@ -38,24 +66,35 @@ JsObject onBeforeRequestCallback(JsObject data) {
   return new JsObject.jsify(response);
 }
 
-void tabsOnUpdatedCallback(int tabId, JsObject changeInfo, JsObject tab) {
-  var status = changeInfo['status'];
-  var url = changeInfo['url'];
-  print('''
-  tab change:
-    id: $tabId
-    url: $url
-    status: $status
-  ''');
-
-  if (status == 'complete') {
-    context['chrome']['tabs'].callMethod('executeScript', [
-        tabId, new JsObject.jsify({
-          'code': '''
-            window.history.replaceState({}, null, "http://gateway.ipfs.io/yummies");
-            system.log('hi! world!');
-          '''
-        }), null
-    ]);
-  }
+void onInstalledAction(JsObject details) {
+  JsObject declarativeContentNamespace = context['chrome']['declarativeContent'];
+  var rules = new JsObject.jsify([{
+    'conditions': [
+      new JsObject(declarativeContentNamespace['PageStateMatcher'], [{
+        'pageUrl': {
+          'originAndPathMatches': '^localhost(:[[:digit:]]+)?\/(ipfs|ipns)\/.+',
+          'schemes': ['http']
+      }}])
+    ],
+    'actions': [
+      new JsObject(declarativeContentNamespace['ShowPageAction']),
+      addContextMenuAction
+    ]
+  }]);
+  declarativeContentNamespace['onPageChanged'].callMethod('removeRules', [
+    null, () {
+    declarativeContentNamespace['onPageChanged'].callMethod('addRules', [rules]);
+  }]);
 }
+
+void pageActionOnClickedAction(JsObject tab) {
+  // Yes this is a zany hack.
+  var tempEl = document.createElement('textarea');
+  document.body.append(tempEl);
+  tempEl.focus();
+  tempEl.select();
+  tempEl.value = tab['url'];
+  document.execCommand('copy', false, null);
+  tempEl.remove();
+}
+
